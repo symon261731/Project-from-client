4a9c37af62b030a33d8d58f8957eb174689c7daa
Simon Mishchenko <symon261731@gmail.com>
2022-10-19 13:56:30 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import express from 'express';
+import morgan from 'morgan';
+import session from 'express-session';
+import store from 'session-file-store';
+import path from 'path';
+import jsxRender from './utils/jsxRender';
+
+require('dotenv').config();
+
+const PORT = 3000;
+const app = express();
+const FileStore = store(session);
+
+app.engine('jsx', jsxRender);
+app.set('view engine', 'jsx');
+app.set('views', path.join(__dirname, 'components'));
+
+const sessionConfig = {
+  name: 'user_sid',
+  secret: process.env.SESSION_SECRET ?? 'test',
+  resave: true,
+  store: new FileStore(),
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 12,
+    httpOnly: true,
+  },
+};
+
+app.use((req, res, next) => {
+  res.locals.path = req.originalUrl;
+  res.locals.bla = 'BLABLA';
+  next();
+});
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session(sessionConfig));
+
+// app.use();
+
+app.listen(PORT, () => console.log(`App has started on port ${PORT}`));
