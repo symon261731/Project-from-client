830876a6d888e298ea17d45c0784815cf93b2eed
Simon Mishchenko <symon261731@gmail.com>
2022-10-19 23:02:17 +0300

    add calendar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Calendar from 'react-calendar';
 import Cards from './Cards';
 import Navbar from './Navbar';
 import Reg from './registration/Reg';
 import Auth from './registration/Auth';
 
 export default function App({ productDB, user }) {
+  const [calendarValue, onChangeCalendar] = useState(new Date());
   const [currentUser, setCurrentUser] = useState(user || null);
   return (
     <div className="container">
@@ -16,6 +18,7 @@ export default function App({ productDB, user }) {
         <Route path="/reg" element={<Reg setCurrentUser={setCurrentUser} />} />
         <Route path="/auth" element={<Auth setCurrentUser={setCurrentUser} />} />
       </Routes>
+      <Calendar className="calendar" onChange={onChangeCalendar} value={calendarValue} />
     </div>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,6 +16,7 @@ export default function Layout({ initState }) {
             __html: `window.initState=${JSON.stringify(initState)}`,
           }}
         />
+        <link rel="stylesheet" href="css/Calendar.css" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossOrigin="anonymous" />
         <script defer src="/app.js" />
