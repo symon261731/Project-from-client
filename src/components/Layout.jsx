d5e3a346bb2dd13b38e01a84d86c1413dbddf6c1
Denis <rule.34@icloud.com>
2022-10-20 11:43:35 +0300

    style 1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -17,6 +17,8 @@ export default function Layout({ initState }) {
           }}
         />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="style.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossOrigin="anonymous" />
         <script defer src="/app.js" />
         <script defer src="/vendor.js" />
